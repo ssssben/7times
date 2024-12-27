b03746ff3ac13f44e0d030c45a621d4a93609057
bun <vlongbf@gmail.com>
2024-12-27 17:59:29 +0900

    Initial commit of mypkg package

---
+++ mypkg/talker.py
@@ -0,0 +1,20 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int16
+
+class Talker(Node):
+    def __init__(self):
+        super().__init__("talker")
+        self.pub = self.create_publisher(Int16, "countup", 10)
+        self.create_timer(0.5,self.cb)
+        self.n = 0
+    def cb(self):
+        msg = Int16()
+        msg.data = self.n
+        self.pub.publish(msg)
+        self.n += 1
+
+def main():
+    rclpy.init()
+    node = Talker()
+    rclpy.spin(node)
